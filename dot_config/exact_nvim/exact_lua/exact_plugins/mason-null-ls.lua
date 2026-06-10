849bf9f33d1bbcefd1acb6ce2c76612ba398882c
k0ch4nx <95961982+k0ch4nx@users.noreply.github.com>
2026-06-11 00:57:08 +0900

    feat: update

--- dot_config/exact_nvim/exact_lua/exact_plugins/mason-null-ls.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/mason-null-ls.lua
@@ -14,6 +14,7 @@ return {
             ensure_installed = {
                 "buildifier",
                 "checkstyle",
+                "csharpier",
                 "google-java-format",
                 "hadolint",
                 "oxfmt",
