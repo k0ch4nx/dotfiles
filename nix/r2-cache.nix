38ac96d322dc7b0480588606fdd2baa602a3d0fe
k0ch4nx <95961982+k0ch4nx@users.noreply.github.com>
2026-07-23 16:33:50 +0900

    CIとローカルのキャッシュ設定を共通化

--- nix/r2-cache.nix
+++ nix/r2-cache.nix
@@ -1,6 +1,8 @@
 let
   accountId = "6118f982b348f7b37129655ee4160301";
   bucket = "nix-cache";
+  accessKeyFile = ../secrets/r2-access-key-id.age;
+  secretKeyFile = ../secrets/r2-secret-access-key.age;
   url = "s3://${bucket}?endpoint=${accountId}.r2.cloudflarestorage.com&scheme=https&region=auto&priority=30";
   localPublicKey = "nix-cache-local:GpHBxUjXDkgtfjKeAD/cuGY8pnCjSsZhc8plkslpfFk=";
   ciPublicKey = "nix-cache-ci:8fZtfHt16O6CvXJlPH0H4uqHTs61K5iruLvTAIFIPmU=";
@@ -15,29 +17,6 @@ let
     localPublicKey
     ciPublicKey
   ];
-in
-{
-  inherit
-    accountId
-    bucket
-    ciPublicKey
-    localPublicKey
-    substituters
-    trustedPublicKeys
-    url
-    ;
-
-  settings = {
-    inherit substituters;
-    trusted-public-keys = trustedPublicKeys;
-    fallback = true;
-  };
-
-  rootCredentialsFile = {
-    darwin = "/var/root/.aws/credentials";
-    linux = "/root/.aws/credentials";
-  };
-
   credentialsGenerator =
     {
       accessKeySecret,
@@ -70,5 +49,83 @@ in
             "$accessKeyId" \
             "$secretAccessKey"
         '';
+    };
+in
+{
+  inherit
+    accountId
+    bucket
+    ciPublicKey
+    localPublicKey
+    substituters
+    trustedPublicKeys
+    url
+    ;
+
+  isGitHubActions = builtins.getEnv "GITHUB_ACTIONS" == "true";
+
+  settings = {
+    inherit substituters;
+    trusted-public-keys = trustedPublicKeys;
+    fallback = true;
+  };
+
+  hosts = {
+    macbook-pro = {
+      credentialsFile = "/var/root/.aws/credentials";
+      credentialsGroup = "wheel";
+    };
+
+    ubuntu-wsl = {
+      credentialsFile = "/root/.aws/credentials";
+      credentialsGroup = "root";
+    };
+  };
+
+  secretAssertions = [
+    {
+      assertion = builtins.pathExists accessKeyFile;
+      message = "The R2 access key ID age file does not exist.";
+    }
+    {
+      assertion = builtins.pathExists secretKeyFile;
+      message = "The R2 secret access key age file does not exist.";
+    }
+  ];
+
+  mkNixSettings = lib: {
+    substituters = lib.mkForce substituters;
+    trusted-public-keys = lib.mkForce trustedPublicKeys;
+    fallback = true;
+  };
+
+  mkCredentialsSecrets =
+    {
+      config,
+      credentialsFile,
+      group,
+    }:
+    {
+      r2-root-access-key-id = {
+        rekeyFile = accessKeyFile;
+        intermediary = true;
+      };
+
+      r2-root-secret-access-key = {
+        rekeyFile = secretKeyFile;
+        intermediary = true;
+      };
+
+      r2-root-credentials = {
+        rekeyFile = ../secrets/r2-credentials.age;
+        generator = credentialsGenerator {
+          accessKeySecret = config.age.secrets.r2-root-access-key-id;
+          secretKeySecret = config.age.secrets.r2-root-secret-access-key;
+        };
+        path = credentialsFile;
+        owner = "root";
+        inherit group;
+        mode = "600";
+      };
     };
 }
