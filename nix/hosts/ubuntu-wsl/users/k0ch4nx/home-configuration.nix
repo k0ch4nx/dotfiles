54178e6ebf218debbd128149b77deb17f2f7a552
k0ch4nx <95961982+k0ch4nx@users.noreply.github.com>
2026-08-06 12:43:54 +0900

    fix

--- nix/hosts/ubuntu-wsl/users/k0ch4nx/home-configuration.nix
+++ nix/hosts/ubuntu-wsl/users/k0ch4nx/home-configuration.nix
@@ -26,7 +26,7 @@
   home.activation.activateAgenixInteractively =
     lib.hm.dag.entryAfter [ "reloadSystemd" ] ''
       PATH=${pkgs.age-plugin-yubikey}/bin:$PATH \
-        ${config.systemd.user.services.agenix.Service.ExecStart}
+        ${builtins.head config.systemd.user.services.agenix.Service.ExecStart}
 
       systemctl --user reset-failed agenix.service || true
     '';
