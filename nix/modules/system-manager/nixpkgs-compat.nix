95cdd8d131ff85340b2ec6b603b0ec923dcd45c5
k0ch4nx <95961982+k0ch4nx@users.noreply.github.com>
2026-08-07 14:10:55 +0900

    fix

--- nix/modules/system-manager/nixpkgs-compat.nix
+++ nix/modules/system-manager/nixpkgs-compat.nix
@@ -1,9 +1,22 @@
-{ lib, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 
 {
+  disabledModules = [
+    "${inputs.system-manager}/nix/modules/upstream/nixpkgs/nix.nix"
+  ];
+
   options.services.displayManager.hiddenUsers = lib.mkOption {
     type = lib.types.listOf lib.types.str;
     default = [ ];
     internal = true;
+  };
+
+  config = lib.mkIf config.nix.enable {
+    environment.etc."nix/nix.conf".replaceExisting = true;
   };
 }
